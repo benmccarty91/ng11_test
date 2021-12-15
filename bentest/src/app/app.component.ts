1d99724745113ef794272302973d1d36104a7208
Benjamin McCarty <bmccarty2@paylocity.com>
2021-12-15 14:09:57 -0700

    ng 11 cleanup

--- bentest/src/app/app.component.ts
+++ bentest/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>ng10 Bentest Client</h1><router-outlet></router-outlet>`,
+  template: `<h1>ng11 Bentest Client</h1><router-outlet></router-outlet>`,
   styles: [``]
 })
 export class AppComponent {
